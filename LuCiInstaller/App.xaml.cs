e251f9c811f83d1ea644c3bb7fd241841d79f6e0
Duy Tân Phùng <phungduytan59nuce@gmail.com>
2024-09-19 20:33:10 +0700

    no message

--- LuCiInstaller/App.xaml.cs
+++ LuCiInstaller/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Microsoft.Data.SqlClient;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,28 @@ namespace LuCiInstaller
      /// </summary>
      public partial class App : Application
      {
+          public static string ConnectionString = "Server=mssql-183717-0.cloudclusters.net,10085;Database=LuCiBimData;User Id=admin;Password=Duytan59nuce;";
+
+          protected override void OnStartup(StartupEventArgs e)
+          {
+               base.OnStartup(e);
+
+               // Bạn cũng có thể cấu hình chuỗi kết nối ở đây nếu cần
+               // string connectionString = "Server=your_server_name;Database=your_database_name;User Id=your_username;Password=your_password;";
+
+               using (SqlConnection connection = new SqlConnection(ConnectionString))
+               {
+                    try
+                    {
+                         connection.Open();
+                         // Thực hiện các thao tác với cơ sở dữ liệu tại đây nếu cần
+                    }
+                    catch (Exception ex)
+                    {
+                         MessageBox.Show("Failed to connect to database: " + ex.Message);
+                    }
+               }
+          }
      }
 
 }
